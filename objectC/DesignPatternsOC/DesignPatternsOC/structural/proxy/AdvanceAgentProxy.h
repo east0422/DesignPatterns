e9fc43de86c7fdc98689960da32b8a98c445a3e3
east <694292191@qq.com>
2021-08-11 22:38:36 +0800

    add protocol

---
+++ objectC/DesignPatternsOC/DesignPatternsOC/structural/proxy/AdvanceAgentProxy.h
@@ -0,0 +1,27 @@
+//
+//  AdvanceAgentProxy.h
+//  DesignPatternsOC
+//
+//  Created by dfang on 2021-8-11.
+//  Copyright © 2021 east. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol AdvanceAgentProxy <NSObject>
+
+@required
+- (void)buy:(NSString *)goods withTip:(CGFloat)tip;
+- (void)sell:(NSString *)goods withTip:(CGFloat)tip;
+
+@optional
+- (void)buyVegetableAndCook:(NSString *)goods forMonthSalary:(CGFloat)salary;
+- (void)buy:(NSString *)goods withTip:(CGFloat)tip forBestChoice:(NSString *)choice;
+- (void)sell:(NSString *)goods withTip:(CGFloat)tip forBestChoice:(NSString *)choice;
+
+@end
+
+NS_ASSUME_NONNULL_END
